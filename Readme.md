2249f009262793dd842506d1da701f35f0e96272
Mirsaid <mir21.07.2004@gmail.com>
2025-09-01 16:54:02 +0500

    init: tutorial app structure

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
-## Redux tutorial
+# Redux tutorial 
+
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Intro from "./1.redux-toolkit-intro";
+import AppStructureAndDataFlow from "./2.app-structure-and-data-flow";
+
+function App() {
+  return (<>
+    <Intro />
+    <AppStructureAndDataFlow />
+  </>);
+}
+
+
+export default App
